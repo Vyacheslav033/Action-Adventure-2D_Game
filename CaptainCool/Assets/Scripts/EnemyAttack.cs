98a12271b49cc300269cadd4c56f70914e10fe9a
Вячеслав Украинцев <slavalapin033@gmail.con>
2021-12-03 19:32:43 +0300

    save game

---
+++ CaptainCool/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{    
+    Animator animator;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameObject.GetComponent<Collider2D>().enabled = false;
+        animator = collision.gameObject.GetComponent<Animator>();
+        animator.SetBool("IsDead", true);
+    }
+
+}
